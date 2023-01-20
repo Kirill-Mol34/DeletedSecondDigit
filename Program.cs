9fb94e09b9b9fc1e536d33e8c9b2937a09f63d9b
Kirill <kir.molokoff@yandex.ru>
2023-01-20 19:07:01 +0900

    final

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int num = new Random().Next(100,999);
+int NewNum = 0;
+int NumWithoutSecDig()
+{
+    NewNum = num / 100 * 10 + num % 10;
+    return NewNum;
+}
+NumWithoutSecDig();
+System.Console.WriteLine($"После удаления второй цифры из чмсла {num},получится {NewNum}");
